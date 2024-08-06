1eb3f15e3758567bb8b9539bdfdc7ac33ed82c24
Santosh Parmar <sonuparmar39@gmail.com>
2024-08-07 01:03:06 +0530

    NuxtJs & Tailwind CSS demo setup

---
+++ pages/about-us.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello, Vue!"
+    };
+  }
+};
+</script>
+<template>
+  <div>
+    <h1>About us Page</h1>
+  </div>
+</template>
+<style scoped>
+h1 {
+  color: blue;
+}
+p {
+  font-size: 18px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello, Vue!"
+    };
+  }
+};
+</script>
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline text-pink-800">Home Page</h1>
+  </div>
+</template>
+<style scoped>
+h1 {
+  color: red;
+}
+p {
+  font-size: 18px;
+}
+</style>
