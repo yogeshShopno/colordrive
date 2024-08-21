3dece77f38f9e0af2c8f8f87846c4bb4946aea2a
Santosh Parmar <sonuparmar39@gmail.com>
2024-08-21 23:48:55 +0530

    Dec code moved in /app successfully build and preview

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
+    <NuxtLayout>
     <NuxtPage />
+  </NuxtLayout>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  srcDir: 'app',
+  buildDir: 'dist/',
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
@@ -9,4 +11,6 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+
+  modules: ["@nuxt/image"]
 })
